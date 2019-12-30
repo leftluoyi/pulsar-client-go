5921bd695b2fab878b2fd62b4a80b9475ac25eea
leftluoyi <leftluoyi@live.cn>
2019-12-30 17:50:26 +0800

    add UpdateTopic for producer

---
+++ docker-ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -e -x
+
+# Project code style check
+docker run --rm -v $(pwd):/app -w /app golangci/golangci-lint:v1.21.0 golangci-lint run -v -c ./.golangci.yml ./...
+
+SRC_DIR=$(git rev-parse --show-toplevel)
+cd ${SRC_DIR}
+
+IMAGE_NAME=pulsar-client-go-test:latest
+
+docker build -t ${IMAGE_NAME} .
+
+docker run -i -v ${PWD}:/pulsar-client-go ${IMAGE_NAME} \
+       bash -c "cd /pulsar-client-go && ./run-ci.sh"
